14db42470ddc0bf4fa7d8ec140920b05f289eb8c
TonyWang <iamtonywang@kakao.com>
2026-03-30 16:13:34 +0900

    Revert divider line changes in Home

--- src/components/sections/Home/Home.tsx
+++ src/components/sections/Home/Home.tsx
@@ -7,7 +7,6 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      <div className={styles.homeDividerLine} aria-hidden="true" />
       <TextSection />
       <VisualSection />
     </>
