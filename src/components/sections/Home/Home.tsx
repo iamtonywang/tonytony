b3c56e641c42057e45496b748864921e17e18000
TonyWang <iamtonywang@kakao.com>
2026-03-31 12:18:46 +0900

    Remove empty VisualSection from Home render flow

--- src/components/sections/Home/Home.tsx
+++ src/components/sections/Home/Home.tsx
@@ -1,14 +1,13 @@
 import styles from "./Home.module.css";
 import HeroSection from "./HeroSection";
 import TextSection from "./TextSection";
-import VisualSection from "./VisualSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <div className={styles.homeDividerLine} aria-hidden="true" />
       <TextSection />
-      <VisualSection />
     </>
   );
 }
